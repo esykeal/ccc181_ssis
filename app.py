d9750e937b482be4b5e3386a19f319444c519c1c
Binsi <vinceeezekiel.jandayan@g.msuiit.edu.ph>
2025-09-18 19:41:02 +0800

    adding login page

--- app.py
+++ app.py
@@ -1,34 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("base.html")
-
-@app.route('/hello')
-def hello():
     return "Hello World"
 
-@app.route('/greet/<name>')
-def greet(name):
-    return f"Hello {name}"
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
 
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    number3 = (number1 + number2)
-    return f"Sum is {number3}"
+        username = request.form['username']
+        password = request.form['password'] 
 
-@app.route('/handle_url_params')
-def handle_params():
-    params = request.args
-    if 'greeting' in params.keys() and 'name' in params.keys():
-        greeting = request.args.get('greeting')
-        name = request.args.get('name')
-        return f"{greeting} {name}"
-    else:
-        return 'Missing some parameters'
-        
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
